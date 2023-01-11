db09fc2e17dcc02cd9f417b2da94d193636a91f4
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-01-11 14:05:08 +0200

    Add files via upload

---
+++ src/main/graphs_dijkstraI_and_mst_lab/dijkstra/KruskalAlgorithm.java
@@ -0,0 +1,14 @@
+package AlgorithmsAdvanced.graphs_dijkstraI_and_mst_lab.dijkstra;
+
+import java.util.List;
+
+public class KruskalAlgorithm {
+
+    public static List<Edge> kruskal(int numberOfVertices, List<Edge> edges) {
+       return null;
+    }
+
+    public static int findRoot(int node, int[] parents) {
+        return 0;
+    }
+}
